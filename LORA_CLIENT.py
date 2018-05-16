484c8fcc446b88555b882d63a4fdcd102e8a76e9
Rui Silva <ruisilva.real@sapo.pt>
2018-05-16 19:27:00 +0100

    Update LORA_CLIENT.py

--- LORA_CLIENT.py
+++ LORA_CLIENT.py
@@ -44,14 +44,14 @@ class mylora(LoRa):
         payload = self.read_payload(nocheck=True )# Receive INF
         print ("Receive: ")
         mens=bytes(payload).decode("utf-8",'ignore')
-        mens=mens[2:] #to discard \x00\x00
+        mens=mens[2:-1] #to discard \x00\x00 and \x00 at the end
         print(mens)
         BOARD.led_off()
         if mens=="INF":
             print("Received data request INF")
             time.sleep(2)
             print ("Send mens: DATA RASPBERRY PI")
-            self.write_payload([255, 255, 0, 0, 68, 65, 84, 65, 32, 82, 65, 83, 80, 66, 69, 82, 82, 89, 32, 80, 73]) # Send DATA RASPBERRY PI
+            self.write_payload([255, 255, 0, 0, 68, 65, 84, 65, 32, 82, 65, 83, 80, 66, 69, 82, 82, 89, 32, 80, 73, 0]) # Send DATA RASPBERRY PI
             self.set_mode(MODE.TX)
         time.sleep(.5)
         self.reset_ptr_rx()
