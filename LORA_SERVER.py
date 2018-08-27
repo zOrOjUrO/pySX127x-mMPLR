d6f98a3c671e72f6f616d1dd5aead26b5076435d
Rui Silva <ruisilva.real@sapo.pt>
2018-08-27 16:44:03 +0100

    Update LORA_SERVER.py

--- LORA_SERVER.py
+++ LORA_SERVER.py
@@ -98,8 +98,18 @@ class mylora(LoRa):
 lora = mylora(verbose=False)
 #args = parser.parse_args(lora) # configs in LoRaArgumentParser.py
 
-#lora.set_mode(MODE.STDBY)
-lora.set_pa_config(pa_select=1)
+#     Slow+long range  Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. 13 dBm
+lora.set_pa_config(pa_select=1, max_power=21, output_power=15)
+lora.set_bw(BW.BW125)
+lora.set_coding_rate(CODING_RATE.CR4_8)
+lora.set_spreading_factor(12)
+lora.set_rx_crc(True)
+#lora.set_lna_gain(GAIN.G1)
+#lora.set_implicit_header_mode(False)
+lora.set_low_data_rate_optim(True)
+
+#  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
+#lora.set_pa_config(pa_select=1)
 
 
 assert(lora.get_agc_auto_on() == 1)
